d511d3bcadbfb0b0de5921c7cec1d4ae9415a4b9
Vasss <git@vasss.ru>
2024-09-02 11:32:56 +0300

    install esbuilder and axios

---
+++ config/esbuild.js
@@ -0,0 +1,10 @@
+const ESBuild = require('esbuild');
+const path = require('path');
+
+ESBuild.build({
+    outdir: path.resolve(__dirname, '..', 'build'),
+    entryPoints: [ path.resolve(__dirname, '..', 'src', 'index.js') ],
+    entryNames: 'script',
+    bundle: true,
+    minify: false
+});
---
+++ src/index.js
@@ -0,0 +1,5 @@
+function test() {
+    console.log('Application GAME 1');
+}
+
+test();
